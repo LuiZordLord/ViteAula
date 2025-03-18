74b8a35a5446208d5d04fc05ec68c6eda3df036d
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2025-03-18 20:29:10 -0300

    Update Hooks.jsx

--- Hooks.jsx
+++ Hooks.jsx
@@ -1,4 +1,5 @@
 import { use, useState } from 'react'
+import { v4 as uuid } from 'uuid'
 import './App.css'
 
 function App() {
@@ -14,12 +15,33 @@ function App() {
   //... Vai percorrer a listaTarefas e recriar a lista com as tarefas exitentes
 
   const adicionarTarefa = () => {
-    setListaTarefas([...listaTarefas, { id: new Date(), nome: nomeTarefa, status: false }])
+
+    if(nomeTarefa.trim() == "") {
+      alert('Adicione uma tarefa!')
+      return
+    }
+
+    setListaTarefas([...listaTarefas, { id: uuid(), nome: nomeTarefa, status: false }])
 
     //Limpar o nome da nova tarefa
     setNomeTarefa('')
 
   }
+
+  const excluirTarefa = (id) => {
+
+    // !== é diferente
+    setListaTarefas(listaTarefas.filter(tarefa => tarefa.id !== id))
+
+  }
+
+  const marcarTarefa = (id) => {
+    setListaTarefas(listaTarefas.map(tarefa => 
+      tarefa.id == id ? {...tarefa, status: !tarefa.status} : tarefa
+
+    ))
+  
+  }  
 
   return (
     <div className="todo-container">
@@ -36,13 +58,13 @@ function App() {
 
       <ul>
 
-        {listaTarefas.map( (Tarefa) => (
+        {listaTarefas.map( (tarefa) => (
 
-          <li key={Tarefa.id}>
-            {Tarefa.nome}
+          <li key={tarefa.id} className={tarefa.status ? "completed" : ''}>
+            {tarefa.nome}
             <div>
-              <button className="complete-btn" >✔</button>
-              <button className="delete-btn" >❌</button>
+              <button className="complete-btn" onClick={() => marcarTarefa(tarefa.id)}>✔</button>
+              <button className="delete-btn" onClick={() => excluirTarefa(tarefa.id)} >❌</button>
             </div>
           </li>
 
@@ -54,6 +76,7 @@ function App() {
 }
 
 export default App
+
 
 .todo-container {
   max-width: 400px;
@@ -133,3 +156,4 @@ li.completed {
   background-color: #dc3545;
   color: white;
 }
+
